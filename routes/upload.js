cb783e41b03ecad7f58f1155d1c178f7156c9aca
Mohamed <hamzatricks@gmail.com>
2026-04-27 17:58:37 +0100

    first

--- routes/upload.js
+++ routes/upload.js
@@ -8,18 +8,21 @@ const {
   uploadMixedFiles,
   deleteFile
 } = require('../controllers/uploadController');
-const { uploadImage, uploadVideo, uploadMixed } = require('../config/cloudinary');
+const cloudinaryConfig = require('../config/cloudinary');
+
+// Use fallback upload middleware since Cloudinary exports were changed
+const uploadFallback = cloudinaryConfig.uploadFallback;
 
 // Image upload routes
-router.post('/image/single', uploadImage.single('image'), uploadSingleImage);
-router.post('/image/multiple', uploadImage.array('images', 10), uploadMultipleImages);
+router.post('/image/single', uploadFallback.single('image'), uploadSingleImage);
+router.post('/image/multiple', uploadFallback.array('images', 10), uploadMultipleImages);
 
 // Video upload routes
-router.post('/video/single', uploadVideo.single('video'), uploadSingleVideo);
-router.post('/video/multiple', uploadVideo.array('videos', 5), uploadMultipleVideos);
+router.post('/video/single', uploadFallback.single('video'), uploadSingleVideo);
+router.post('/video/multiple', uploadFallback.array('videos', 5), uploadMultipleVideos);
 
 // Mixed files upload (both images and videos)
-router.post('/mixed', uploadMixed.array('files', 15), uploadMixedFiles);
+router.post('/mixed', uploadFallback.array('files', 15), uploadMixedFiles);
 
 // Delete file
 router.delete('/delete', deleteFile);
