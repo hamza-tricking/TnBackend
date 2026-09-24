b7d951e5188bbdfb3f272a9301cce7c27f597c7a
Mohamed <hamzatricks@gmail.com>
2026-09-24 15:02:44 +0100

    feat: display order ID in Yalidine Designation instead of empty string

--- services/yalidineService.js
+++ services/yalidineService.js
@@ -313,7 +313,7 @@ class YalidineService {
       address: order.shippingAddress.street || order.shippingAddress.baladiya || normalizedCommune || '.',
       to_commune_name: normalizedCommune,
       to_wilaya_name: normalizedWilaya,
-      product_list: '',
+      product_list: order.orderNumber || (order._id ? `TN-${order._id.toString().slice(-6).toUpperCase()}` : 'TN-ORDER'),
       price: order.total,
       freeshipping: false,
       is_stopdesk: isStopdesk,
